7d47d1dc4c9200a8576766c0c1923f8a06d0f42a
Apoerakay <apoerak@gmail.com>
2024-03-13 17:07:49 +0000

    connected to database

--- src/components/create-todo/index.js
+++ src/components/create-todo/index.js
@@ -1,35 +1,47 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 import { useLocalStorage } from "usehooks-ts";
 
 function CreateTodo() {
-    const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
+  const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
 
-    const[todo, setTodo] = useState("");
+  const [todo, setTodo] = useState("");
 
-    const saveTodos = () => {
-// Save all todos
-        setTodos([...todos, todo]);
-// Wipe the input box
-        setTodo("");
-    }
+  const saveTodos = async () => {
+    // post  todos to todo-api
+    const response = await fetch("http://localhost:4000/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        title: todo,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data)
+    // Wipe the input box
+    setTodo("");
+  };
 
-   
-    return(
-        <section className={styles.CreateTodoSection}> 
-        <input 
+  return (
+    <section className={styles.CreateTodoSection}>
+      <input
         value={todo} // clears after typing
         // onkey down enables enter to create todos
-        onKeyDown={event=> event.key == "Enter" && saveTodos()}
-        onChange={event=>setTodo(event.target.value)}
-         className={styles.createTodoInput}
-        placeholder="start typing..."/>
-        <button 
-        onClick={()=> saveTodos()} 
-        className={'btn btn-primary ${styles.btn}'}
-        >create</button>
-        </section>
-    );
+        onKeyDown={(event) => event.key == "Enter" && saveTodos()}
+        onChange={(event) => setTodo(event.target.value)}
+        className={styles.createTodoInput}
+        placeholder="start typing..."
+      />
+      <button
+        onClick={() => saveTodos()}
+        className={"btn btn-primary ${styles.btn}"}
+      >
+        create
+      </button>
+    </section>
+  );
 }
 
 export default CreateTodo;
